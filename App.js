cfed6950dcd090b9e266d29092651bae971ff097
NikhilPatra0407 <nick0407@gmail.com>
2025-03-17 16:38:14 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child1" },
+    React.createElement("h1", {}, "I'm a h1 tag!")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
